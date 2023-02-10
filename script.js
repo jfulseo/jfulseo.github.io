103cf7dc3fd4b2389d424e462c29270f53f91ac0
jfulseo <37602487+jfulseo@users.noreply.github.com>
2023-02-10 11:29:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ window.onload = () => {
 
     el.addEventListener("gps-camera-update-position", e => {
         
-        //alert(e.detail.position.latitude +", "+testEntityAdded);
+        const dist = getDistanceFromLatLonInKm(35.2664,129.0931,e.detail.position.latitude,e.detail.position.longitude);
+        
+        
+        alert(dist);
 
         //console.log(e);
         
@@ -46,24 +49,25 @@ window.onload = () => {
                 compoundEntity.appendChild(text);
                 document.querySelector("a-scene").appendChild(compoundEntity);            
             
-            /*
             
-            entity.setAttribute("scale", {
-                x: 20, 
-                y: 20,
-                z: 20
-            });
-            entity.setAttribute('material', { color: 'red' } );
-            entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude + 0.001,
-                longitude: e.detail.position.longitude
-            });
-            document.querySelector("a-scene").appendChild(entity);
-            */
-            //console.log(e);
         }
         testEntityAdded = true;
         
         
     });
 };
+
+
+function getDistanceFromLatLonInKm(lat1,lng1,lat2,lng2) {
+    function deg2rad(deg) {
+        return deg * (Math.PI/180)
+    }
+
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lng2-lng1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+}
