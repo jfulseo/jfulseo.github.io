d0340e1d4eab17ccde4bf247cb941b8c586627bd
jfulseo <37602487+jfulseo@users.noreply.github.com>
2023-02-10 11:17:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,49 @@ window.onload = () => {
 
     el.addEventListener("gps-camera-update-position", e => {
         
-        alert(e.detail.position.latitude +", "+testEntityAdded);
+        //alert(e.detail.position.latitude +", "+testEntityAdded);
 
         //console.log(e);
         
         if(!testEntityAdded) {
 
             // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-box");
+            //const entity = document.createElement("a-box");
+                const compoundEntity = document.createElement("a-entity");
+                compoundEntity.setAttribute('gps-new-entity-place', {
+                    latitude: e.detail.position.latitude + 0.001,
+                    longitude: e.detail.position.longitude
+                });
+            
+                const box = document.createElement("a-box");
+                box.setAttribute("scale", {
+                    x: 20,
+                    y: 20,
+                    z: 20
+                });
+                box.setAttribute('material', { color: 'red' } );
+                box.setAttribute("position", {
+                    x : 0,
+                    y : 20,
+                    z: 0
+                } );
+            
+                const text = document.createElement("a-text");
+                const textScale = 100;
+                text.setAttribute("look-at", "[gps-new-camera]");
+                text.setAttribute("scale", {
+                    x: textScale,
+                    y: textScale,
+                    z: textScale
+                });
+                text.setAttribute("value", "경남 김해시 사발면 1277");
+                text.setAttribute("align", "center");
+                compoundEntity.appendChild(box);
+                compoundEntity.appendChild(text);
+                document.querySelector("a-scene").appendChild(compoundEntity);            
+            
+            /*
+            
             entity.setAttribute("scale", {
                 x: 20, 
                 y: 20,
@@ -24,7 +59,7 @@ window.onload = () => {
                 longitude: e.detail.position.longitude
             });
             document.querySelector("a-scene").appendChild(entity);
-            
+            */
             //console.log(e);
         }
         testEntityAdded = true;
