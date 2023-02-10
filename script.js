446cb6a89eb192257b335755db621f0e9d67f7f4
jfulseo <37602487+jfulseo@users.noreply.github.com>
2023-02-10 10:33:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
-
 window.onload = () => {
-    const scene = document.querySelector('a-scene');
+    let downloaded = false;
 
-    // first get current user location
-    return navigator.geolocation.getCurrentPosition(function (position) {
+    const el = document.querySelector("[gps-new-camera]");
 
-                    const latitude = 35.2664;
-                    const longitude = 129.0931;
-
-      
-                    // add place name
-                    const placeText = document.createElement('a-link');
-                    placeText.setAttribute('gps-entity-place', 'latitude: ${latitude}; longitude: ${longitude};');
-                    placeText.setAttribute('title', '경상남도 김해시 사발면 12345');
-                    placeText.setAttribute('scale', '15 15 15');
-                    
-                    placeText.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                    });
-                    scene.appendChild(placeText);        
-        
-      
-      /*
-        // than use it to load from remote APIs some places nearby
-        loadPlaces(position.coords)
-            .then((places) => {
-                places.forEach((place) => {
-                    const latitude = place.location.lat;
-                    const longitude = place.location.lng;
-
-                    // add place name
-                    const placeText = document.createElement('a-link');
-                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    placeText.setAttribute('title', place.name);
-                    placeText.setAttribute('scale', '15 15 15');
-                    
-                    placeText.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                    });
-
-                    scene.appendChild(placeText);
+    el.addEventListener("gps-camera-update-position", async(e) => {
+        if(!downloaded) {
+            const west = e.detail.position.longitude - 0.05,
+                  east = e.detail.position.longitude + 0.05,
+                  south = e.detail.position.latitude - 0.05;
+                  north = e.detail.position.latitude + 0.05;
+            console.log(`${west} ${south} ${east} ${north}`);
+            const response = await fetch(`https://hikar.org/webapp/map?bbox=${west},${south},${east},${north}&layers=poi&outProj=4326`);
+            const pois = await response.json();
+            pois.features.forEach ( feature => {
+                const compoundEntity = document.createElement("a-entity");
+                compoundEntity.setAttribute('gps-new-entity-place', {
+                    latitude: feature.geometry.coordinates[1],
+                    longitude: feature.geometry.coordinates[0]
                 });
-            })
-      */
-    },
-        (err) => console.error('Error in retrieving position', err),
-        {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-            timeout: 27000,
+                const box = document.createElement("a-box");
+                box.setAttribute("scale", {
+                    x: 20,
+                    y: 20,
+                    z: 20
+                });
+                box.setAttribute('material', { color: 'red' } );
+                box.setAttribute("position", {
+                    x : 0,
+                    y : 20,
+                    z: 0
+                } );
+                const text = document.createElement("a-text");
+                const textScale = 100;
+                text.setAttribute("look-at", "[gps-new-camera]");
+                text.setAttribute("scale", {
+                    x: textScale,
+                    y: textScale,
+                    z: textScale
+                });
+                text.setAttribute("value", feature.properties.name);
+                text.setAttribute("align", "center");
+                compoundEntity.appendChild(box);
+                compoundEntity.appendChild(text);
+                document.querySelector("a-scene").appendChild(compoundEntity);
+            });
         }
-    );
+        downloaded = true;
+    });
 };
