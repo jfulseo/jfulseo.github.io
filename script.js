13096c4ace0b5ed77f809fae641dfcc43df8d9f8
jfulseo <37602487+jfulseo@users.noreply.github.com>
2023-02-11 13:39:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ window.onload = () => {
             longitude: _longitude
         });
 
-        const box = document.createElement("map_pin");
+        const box = document.createElement("#map_pin");
 
         let _scale = 100;
         let textScale = 600;
